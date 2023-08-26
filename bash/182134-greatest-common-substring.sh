8965bfe6266b9dbe5528f1235dc45fb26d4c7ec2
Rob Papesch <robertpnz@gmail.com>
2023-08-27 08:40:21 +1000

    206 b
    https://codegolf.stackexchange.com/posts/182250/revisions

--- bash/182134-greatest-common-substring.sh
+++ bash/182134-greatest-common-substring.sh
@@ -1,8 +1,7 @@
-for l;{
-for((i=0;i<${#l};i++)){ s="${l: -i}" a+=("$s")
-for((j=1;j<${#s};j++)){ a+=("${s:0:j}");};};}
+for l;{ d=${#l}
+for((;i++<d**2;)){ a+=(${l:1+i/d:1+i%d});};}
 for m in "${a[@]}";do k=0
-for n in "$@";do [[ $n =~ $m ]]&&((k++));done
+for n;{ [[ $n =~ $m ]]&&((k++));}
 ((k==$#))&&b+=("$m");done
 for e in "${b[@]}";do((${#e}>${#f}))&&f="$e";done
 echo "$f"
