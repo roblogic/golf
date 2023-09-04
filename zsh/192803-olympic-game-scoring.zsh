84f50bbbb5764057599fc9f87cf4c19802a6fa69
Rob Papesch <robertpnz@gmail.com>
2023-09-04 17:04:28 +1000

    109 bytes
    https://codegolf.stackexchange.com/questions/192803/olympic-game-scoring/192825#192825

--- zsh/192803-olympic-game-scoring.zsh
+++ zsh/192803-olympic-game-scoring.zsh
@@ -1,5 +1,2 @@
-setopt FORCE_FLOAT
-m=$1 n=$1 a=$#
-for x ((t+=x))&&m=$[x>m?x:m]&&n=$[x<n?x:n]
-s=$[2*(a>2?(t-m-n)/(a-2):t/a)]
-<<<$[(s^0+(s-s^0>.5?1:0))/2]
+m=$1 n=$1 a=$#;for x ((t+=x,m=x>m?x:m,n=x<n?x:n))
+s=$[2*(a>2?(t-m-n)/(a-2):t/a)];<<<$[(s^0+(s-s^0>.5?1:0))/2]
