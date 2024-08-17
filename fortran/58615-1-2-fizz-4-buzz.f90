e7ae47563a12513b5df9fd10913c04da911f691d
Rob Papesch <robertpnz@gmail.com>
2024-08-17 20:40:15 +1000

    128 bytes!!!
    https://codegolf.stackexchange.com/questions/58615/1-2-fizz-4-buzz/254203#254203

--- fortran/58615-1-2-fizz-4-buzz.f90
+++ fortran/58615-1-2-fizz-4-buzz.f90
@@ -1,3 +1,7 @@
-character(8)s;do i=1,100;s=''
-if(mod(i,3)<1)s='fizz';if(mod(i,5)<1)s=trim(s)//'buzz'
-if(s>'')print'(A)',s;if(s<'A')print'(i0)',i;enddo;end
+character*8s;do i=1,100;s=''
+if(mod(i,3)<1)s='fizz'
+if(mod(i,5)<1)s=trim(s)//'buzz'
+if(s=='')write(s,'(i0)')i
+print*,s
+enddo
+end
